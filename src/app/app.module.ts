fc090bd07dc68f7a6ab7ede4ffa8744330e53a9b
Bhargav <bhargavkadali39@gmail.com>
2021-12-24 23:20:02 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import{ AngularFireModule } from '@angular/fire';
-import{ AngularFireDatabaseModule } from '@angular/fire/database';
-import {environment} from '../environments/environment';
-import{ AngularFirestoreModule } from '@angular/fire/firestore';
-
-import{ AngularFirestoreModule } from '@angular/fire/firestore';
-
-import { AngularFireAuthModule } from "@angular/fire/compat/auth";
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -20,33 +11,32 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    AngularFirestoreModule,
-    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
 
+/* 
 
 
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
+import{ AngularFireModule } from '@angular/fire';
+import{ AngularFireDatabaseModule } from '@angular/fire/database';
+import {environment} from '../environments/environment';
+import{ AngularFirestoreModule } from '@angular/fire/firestore';
 
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
+import{ AngularFirestoreModule } from '@angular/fire/firestore';
 
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+
+AngularFireModule.initializeApp(environment.firebase),
+AngularFireDatabaseModule,
+AngularFirestoreModule,
+AngularFireAuthModule
+
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    Firestore,collectionData, collection
+
+*/
