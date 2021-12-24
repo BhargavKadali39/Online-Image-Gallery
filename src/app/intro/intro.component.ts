76cdab9674546e14b0550b75c61845cbab903359
Bhargav <bhargavkadali39@gmail.com>
2021-12-24 23:21:48 +0530

    Update intro.component.ts

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -13,5 +13,16 @@ export class IntroComponent implements OnInit {
   }
 
   a:number = 4;
+  
+  PICTURES: string[] = ["https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg",
+   "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg",
+   "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg"
+  ];
+
+  users = [
+    { name: 'Anderson', age: 35, joinDate: new Date('2005-03-25') },
+    { name: 'John', age: 12, joinDate: new Date('2012-05-09') },
+    { name: 'Peter', age: 22, joinDate: new Date('2014-10-21') }
+  ];
 
 }
