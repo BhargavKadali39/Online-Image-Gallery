ddaa0378f98675426238fa4efedc93923376feb7
Bhargav <bhargavkadali39@gmail.com>
2021-12-27 21:57:30 +0530

    Add files via upload

---
+++ src/app/collection3/collection3.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collection3',
+  templateUrl: './collection3.component.html',
+  styleUrls: ['./collection3.component.scss']
+})
+export class Collection3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  collection1:any = ["https://i.pinimg.com/736x/2b/90/b2/2b90b2409912f779e4a3cb69f7faee48.jpg",
+  "https://i.pinimg.com/originals/af/6a/df/af6adff60ad43273a536a9a0d5b545c2.jpg",
+  "https://i.pinimg.com/736x/f5/ec/4f/f5ec4fbe24d68fbb585bb904c6f3ed35.jpg",
+  "https://i.pinimg.com/originals/3f/49/4a/3f494ac43f779ea4801d47585cd94875.jpg",
+  "https://i.pinimg.com/736x/68/47/8a/68478aa324616ab56e0e9bade3ef204e.jpg",
+  "https://i.pinimg.com/236x/3f/91/9b/3f919b266c031e45ec13141465bca022.jpg",
+  "https://i.pinimg.com/originals/55/64/e7/5564e724996f291c6cb14090cc79687e.jpg",
+  "https://cutewallpaper.org/22/aesthetic-ghost-wallpapers/ghost-aesthetic-wallpaper-tiktok-creds-natenorell-ghost-photography-grunge-photography-grunge-pictures.jpg"
+]
+}
